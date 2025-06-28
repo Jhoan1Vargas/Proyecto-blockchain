57d0817da6f181d094433ff975d2e49ab5dc9843
OmarCR117 <omarcabrera.r117@gmail.com>
2025-06-28 13:13:15 -0400

    Ajustes en barra lateral

--- netherium-app/src/scenes/global/Sidebar.jsx
+++ netherium-app/src/scenes/global/Sidebar.jsx
@@ -9,15 +9,35 @@ import PeopleOutlinedIcon from '@mui/icons-material/PeopleOutlined';
 import ContactsOutlinedIcon from '@mui/icons-material/ContactsOutlined';
 import ReceiptOutlinedIcon from '@mui/icons-material/ReceiptOutlined';
 import PersonOutlinedIcon from '@mui/icons-material/PersonOutlined';
-import HelpOutlinedIcon from '@mui/icons-material/HelpOutlined';
+import HelpOutlineOutlinedIcon from '@mui/icons-material/HelpOutlineOutlined';
 import BarChartOutlinedIcon from '@mui/icons-material/BarChartOutlined';
-import PieChartOuttlinedIcon from '@mui/icons-material/PieChartOutlined';
+import PieChartOutlineOutlinedIcon from '@mui/icons-material/PieChartOutlineOutlined';
 import TimelineOutlinedIcon from '@mui/icons-material/TimelineOutlined';
 import MenuOutlinedIcon from '@mui/icons-material/MenuOutlined';
-import MapOutlinedIcon from '@mui/icons-material/LightModeOutlined';
-import { Padding } from "@mui/icons-material";
+import MapOutlinedIcon from '@mui/icons-material/MapOutlined';
+import {Divider} from "@mui/material"
 
-const Sidebar = () => {
+
+
+const Item = ({ title, to, icon, selected, setSelected }) => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  return (
+    <MenuItem
+      active={selected === title}
+      style={{
+        color: colors.grey[100],
+      }}
+      onClick={() => setSelected(title)}
+      icon={icon}
+    >
+      <Typography>{title}</Typography>
+      <Link to={to} />
+    </MenuItem>
+  );
+};
+
+const Sidebar = ({ username, userrol}) => {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
   const [isCollapsed, setIsCollapsed] = useState(false);
@@ -59,14 +79,149 @@ const Sidebar = () => {
                 display="flex" 
                 justifyContent="space-between" 
                 alignItems="center" ml="15px">
-                <Typography
-                  variant="h4" color={colors.grey[100]}>NETHEREUM</Typography>
+                <Typography variant="h4" color={colors.grey[100]}>
+                  NETHEREUM
+                </Typography>
                 <IconButton onClick={() => setIsCollapsed(!isCollapsed)}>
                   <MenuOutlinedIcon/>
                 </IconButton>
               </Box>
             )}
           </MenuItem>
+
+
+            {/* USER */}
+            {!isCollapsed && (
+              <Box mb="25 px">
+                <Box display="flex" justifyContent="center" alignContent="center">
+                  <img 
+                    alt="profile-user"
+                    width="100px"
+                    height="100px"
+                    src={`.../../assets/user.png`}
+                    style={{cursor: "pointer", borderRadius:"50%"}}
+                  />
+                </Box>
+                <Box textAlign="center"> 
+                  <Typography 
+                    variant="h3" 
+                    color={colors.grey[100]} 
+                    fontWeight="bold" 
+                    sx={{ m: "10px 0 0 0" }}>{username}</Typography>
+                  <Typography 
+                    variant="h6" 
+                    color={colors.greenAccent[500]}
+                  >{userrol}</Typography>
+                </Box>
+              </Box>
+            )}
+
+            <Divider sx={{ my: 2, borderColor: colors.grey[100] }} />
+
+            {/* MENU ITEMS */}
+            <Box paddingLeft={isCollapsed ? undefined : "10%"}>
+              <Item
+                title="Dashboard"
+                to="/"
+                icon={<HomeOutlinedIcon />}
+                selected={selected}
+                setSelected={setSelected}
+              />
+
+              <Typography
+                variant="h6"
+                color={colors.grey[200]}
+
+                sx= {isCollapsed ? {m:"15px 0 5px 22px"} : {m:"15px 0 5px 20px"}}
+              >
+                Data
+              </Typography>
+
+              <Item
+              title="Equipo"
+              to="/team"
+              icon={<PeopleOutlinedIcon />}
+              selected={selected}
+              setSelected={setSelected}
+            />
+
+              <Item
+              title="Contactos"
+              to="/contacts"
+              icon={<ContactsOutlinedIcon />}
+              selected={selected}
+              setSelected={setSelected}
+            />
+            <Item
+              title="Balances"
+              to="/invoices"
+              icon={<ReceiptOutlinedIcon />}
+              selected={selected}
+              setSelected={setSelected}
+            />
+
+            <Typography
+              variant="h6"
+              color={colors.grey[300]}
+              sx= {isCollapsed ? {m:"15px 0 5px 15px"} : {m:"15px 0 5px 20px"}}
+            >
+              Páginas
+            </Typography>
+
+            <Item
+              title="Usuario"
+              to="/form"
+              icon={<PersonOutlinedIcon />}
+              selected={selected}
+              setSelected={setSelected}
+            />
+
+            <Item
+              title="FAQ"
+              to="/faq"
+              icon={<HelpOutlineOutlinedIcon />}
+              selected={selected}
+              setSelected={setSelected}
+            />
+
+            <Typography
+              variant="h6"
+              color={colors.grey[300]}
+              sx= {isCollapsed ? {m:"15px 0 5px 15px"} : {m:"15px 0 5px 20px"}}
+            >
+              Gráficos
+            </Typography>
+            <Item
+              title="Gráfico de Barras"
+              to="/bar"
+              icon={<BarChartOutlinedIcon />}
+              selected={selected}
+              setSelected={setSelected}
+            />
+            <Item
+              title="Gráfico Pastel"
+              to="/pie"
+              icon={<PieChartOutlineOutlinedIcon />}
+              selected={selected}
+              setSelected={setSelected}
+            />
+
+            <Item
+              title="Gráfico de Líneas"
+              to="/line"
+              icon={<TimelineOutlinedIcon />}
+              selected={selected}
+              setSelected={setSelected}
+            />
+            <Item
+              title="Gráfico Geográfico"
+              to="/geography"
+              icon={<MapOutlinedIcon />}
+              selected={selected}
+              setSelected={setSelected}
+            />
+
+          </Box>
         </Menu>
       </ProSidebar>
     </Box>
