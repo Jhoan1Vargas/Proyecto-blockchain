f99b64a0c9205384f8062904afbd84c53475e7bf
OmarCR117 <omarcabrera.r117@gmail.com>
2025-06-28 01:00:44 -0400

    Ajustes de referencias

--- netherium-app/src/scenes/global/Topbar.jsx
+++ netherium-app/src/scenes/global/Topbar.jsx
@@ -1,5 +1,10 @@
+import {Box, IconButton, useTheme} from "@mui/material";
+import { useContext } from "react";
+import { ColorModeContext, tokens } from "../../theme";
+import InputBase from "@mui/material";
+
 const Topbar = () => {
-    return <div>Topbar</div>
+  return <div>Topbar</div>
 }
 
 export default Topbar;
