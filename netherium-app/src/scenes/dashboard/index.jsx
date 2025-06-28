640db057b4812281bc1a63f4715ef0021f425a71
OmarCR117 <omarcabrera.r117@gmail.com>
2025-06-28 11:05:24 -0400

    Barra lateral y comienzo de dashboard

--- netherium-app/src/scenes/dashboard/index.jsx
+++ netherium-app/src/scenes/dashboard/index.jsx
@@ -1,5 +1,12 @@
+import Header from "../../components/Header";
+import { Box } from "@mui/material";
+
 const Dashboard = () => {
-    return <div>Dashboard</div>
-}
+    return <Box m="20px">
+        <Box display="flex" justifyContent="space-between" alignItems="center" >
+            <Header title="DASHBOARD" subtitle="Bienvenido a tu dashboard"></Header>
+        </Box>
+    </Box>;
+};
 
 export default Dashboard;
